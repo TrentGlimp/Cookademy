af417a0c2cdd47aa2173c75ce0086a781d9cdd88
Trent Glimp <chitr8or@gmail.com>
2022-11-17 08:09:34 -0600

    Initial commit

---
+++ Cookademy/CookademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookademyApp.swift
+//  Cookademy
+//
+//  Created by Trent Glimp on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct CookademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
