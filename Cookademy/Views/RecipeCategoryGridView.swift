b43d5b3bb858f252b30943656e3102f78db25e61
Trent Glimp <chitr8or@gmail.com>
2022-11-21 16:35:20 -0600

    Recipes listed by category

--- Cookademy/Views/RecipeCategoryGridView.swift
+++ Cookademy/Views/RecipeCategoryGridView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct RecipeCategoryGridView: View {
-    private var recipeData = RecipeData()
+    @StateObject private var recipeData = RecipeData()
+
+    var body: some View {
+        let columns = [GridItem(), GridItem()]
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, content: {
+                    ForEach(MainInformation.Category.allCases, id: \.self) { category in
+                        NavigationLink(
+                            destination: RecipesListView(category: category).environmentObject(recipeData),
+                            label: {
+                                CategoryView(category: category)
+                            })
+                    }
+                })
+            }
+            .navigationTitle("Categories")
+        }
+    }
+}
+
+struct CategoryView: View {
+    let category: MainInformation.Category
     
     var body: some View {
-        NavigationView {
-            LazyVGrid(columns: [GridItem()], content: {
-                ForEach(MainInformation.Category.allCases, id: \.self) {
-                    category in Text(category.rawValue)
-                        .font(.title)
-                }
-            })
-            .navigationTitle("Categories")
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.35)
+            Text(category.rawValue)
+                .font(.title)
         }
     }
 }
--- Cookademy/Views/RecipesListView.swift
+++ Cookademy/Views/RecipesListView.swift
@@ -8,15 +8,16 @@
 import SwiftUI
 
 struct RecipesListView: View {
-    @StateObject var recipeData = RecipeData()
-    
+    @EnvironmentObject private var recipeData: RecipeData
+    let category: MainInformation.Category
+
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
     
     var body: some View {
         List {
-            ForEach(recipes) {
-                recipe in NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
+            ForEach(recipes) { recipe in
+                NavigationLink(recipe.mainInformation.name, destination: RecipeDetailView(recipe: recipe))
             }
             .listRowBackground(listBackgroundColor)
             .foregroundColor(listTextColor)
@@ -26,19 +27,19 @@ struct RecipesListView: View {
 }
 
 extension RecipesListView {
-    var recipes: [Recipe] {
-        recipeData.recipes
+    private var recipes: [Recipe] {
+        recipeData.recipes(for: category)
     }
     
-    var navigationTitle: String {
-        "All Recipes"
+    private var navigationTitle: String {
+        "\(category.rawValue) Recipes"
     }
 }
 
 struct RecipesListView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            RecipesListView()
-        }
+            RecipesListView(category: .breakfast)
+        }.environmentObject(RecipeData())
     }
 }
