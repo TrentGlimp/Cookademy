d1d0ef1c618f5c4b6fb81983f3ad3e735d18ae02
Trent Glimp <chitr8or@gmail.com>
2022-11-28 16:47:39 -0600

    Final Commit For Submission

--- Cookademy/Views/RecipeDetailView.swift
+++ Cookademy/Views/RecipeDetailView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct RecipeDetailView: View {
     let recipe: Recipe
-    
+
     private let listBackgroundColor = AppColor.background
     private let listTextColor = AppColor.foreground
-    
+
     var body: some View {
         VStack {
             HStack {
@@ -23,21 +23,21 @@ struct RecipeDetailView: View {
             }
             HStack {
                 Text(recipe.mainInformation.description)
-                    .font(.subheadline)
+                    .font(.body)
                     .padding()
                 Spacer()
             }
             List {
                 Section(header: Text("Ingredients")) {
-                    ForEach(recipe.ingredients.indices, id: \.self) {
-                        index in let ingredient = recipe.ingredients[index]
+                    ForEach(recipe.ingredients.indices, id: \.self) { index in
+                        let ingredient = recipe.ingredients[index]
                         Text(ingredient.description)
                             .foregroundColor(listTextColor)
                     }
                 }.listRowBackground(listBackgroundColor)
-                Section(header: Text("Directons")) {
-                    ForEach(recipe.directions.indices, id: \.self) {
-                        index in let direction = recipe.directions[index]
+                Section(header: Text("Directions")) {
+                    ForEach(recipe.directions.indices, id: \.self) { index in
+                        let direction = recipe.directions[index]
                         HStack {
                             Text("\(index + 1). ").bold()
                             Text("\(direction.isOptional ? "(Optional) " : "")\(direction.description)")
